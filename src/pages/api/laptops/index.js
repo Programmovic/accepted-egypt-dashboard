cf14540d279a112a68704c13e8ff778351115607
Programmovic <adamsoliman800@gmail.com>
2024-04-06 23:47:50 +0200

    build(deps): update package-lock.json, package.json, src/pages/api/laptops/index.js and src/pages/inventory/laptops/index.jsx

--- src/pages/api/laptops/index.js
+++ src/pages/api/laptops/index.js
@@ -2,6 +2,10 @@
 import connectDB from "@lib/db";
 import Laptop from "../../../models/laptops";
 import Instructor from "../../../models/instructor";
+import QRCode from "qrcode";
+import { promisify } from "util";
+
+const generateQRCodeAsync = promisify(QRCode.toDataURL);
 
 export default async (req, res) => {
   await connectDB();
@@ -13,10 +17,19 @@ export default async (req, res) => {
       // Create a new Laptop document
       const newLaptop = new Laptop(laptopData);
 
+      // Generate QR code for the laptop
+      const qrCodeURL = await generateQRCodeAsync(JSON.stringify({
+        SerialNumber: newLaptop._id,
+        Brand: newLaptop.brand,
+        Model: newLaptop.model,
+        AssignedTo: newLaptop.assignedTo?.name || 'N/A',
+        AssignedDate: newLaptop.assignedDate || 'N/A',
+      }));
+
       // Save the new laptop record to the database
       await newLaptop.save();
 
-      return res.status(201).json(newLaptop);
+      return res.status(201).json({ ...newLaptop.toJSON(), qrCodeURL });
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: "Could not create Laptop record" });
@@ -24,7 +37,20 @@ export default async (req, res) => {
   } else if (req.method === "GET") {
     try {
       const allLaptops = await Laptop.find().populate("assignedTo", "name"); // Populate the "assignedTo" field with the name of the instructor
-      return res.status(200).json(allLaptops);
+      
+      // Generate QR code URLs for each laptop
+      const laptopsWithQRCode = await Promise.all(allLaptops.map(async (laptop) => {
+        const qrCodeURL = await generateQRCodeAsync(JSON.stringify({
+          SerialNumber: laptop._id,
+          Brand: laptop.brand,
+          Model: laptop.model,
+          AssignedTo: laptop.assignedTo?.name || 'N/A',
+          AssignedDate: laptop.assignedDate || 'N/A',
+        }));
+        return { ...laptop.toJSON(), qrCodeURL };
+      }));
+console.log(laptopsWithQRCode)
+      return res.status(200).json(laptopsWithQRCode);
     } catch (error) {
       console.error(error);
       return res.status(500).json({ error: "Could not fetch Laptop records" });
