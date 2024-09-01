7343479245d94aada127ba2124391590a39862c7
Programmovic <adamforbusiness@outlook.com>
2024-09-01 20:21:50 +0300

    update src/components/UploadExcel/index.jsx

--- src/components/UploadExcel/index.jsx
+++ src/components/UploadExcel/index.jsx
@@ -9,17 +9,21 @@ const ExcelUploadDownload = ({ handleDownloadTemplate, handleDataUpload }) => {
       <Form className="my-3 px-4 pt-3 pb-1" style={{ backgroundColor: "rgb(245, 245, 245)", borderRadius: "8px" }}>
         <Row className="mb-3">
 
-          <Col xs={9}>
-            <Form.Group controlId="fileUpload">
+          <Col xs={9} className='d-flex align-items-end '>
+            <Form.Group controlId="fileUpload" className='w-100'>
               <Form.Label>Upload Excel File</Form.Label>
               <Form.Control type="file" accept=".xlsx, .xls" onChange={handleDataUpload} />
             </Form.Group>
           </Col>
-          <Col className='d-flex align-items-end'>
-            <Button variant="dark" onClick={handleDownloadTemplate} className="w-100">
-              Download Excel Template
-            </Button>
-          </Col>
+          <Col className="d-flex align-items-end flex-column">
+  <Button variant="dark" onClick={handleDownloadTemplate} className="w-100 mb-2">
+    Create New Lead
+  </Button>
+  <Button variant="dark" onClick={handleDownloadTemplate} className="w-100">
+    Download Excel Template
+  </Button>
+</Col>
+
         </Row>
       </Form>
     </>
